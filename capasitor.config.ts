35b52594199f89b9ecc090b310161667e4e2b460
Iyan-Core <awianton2@gmail.com>
2026-06-11 07:23:26 +0700

    Create capasitor.config.ts

---
+++ capasitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.katalogpro.app',
+  appName: 'KatalogPro',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
